fa718a1f9a4ceaf6d2483c2f3b84d76cd41bae83
terryproctor <terryproctor2015@outlook.com>
2021-03-07 22:05:52 +0000

    repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = (string,times) => {
+   if (times >=0) {
+    return string.repeat(times);
+   }
+   else {
+       return 'ERROR';
+   }
 }
-
 module.exports = repeatString
