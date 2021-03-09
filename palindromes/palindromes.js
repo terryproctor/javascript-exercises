a2a2f80b9133dfd749f5d611f7eb4b3d666d5dcc
terryproctor <terryproctor2015@outlook.com>
2021-03-09 20:36:44 +0000

    final

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function() {
+palindromes = (string) => {
+    //remove spaces and special chars
+    let pattern = /\W/ig;
+    let removedString = string.toLowerCase().replace(pattern, '');
+    
+    //reverse removedstring
+    reverseString = removedString.split('').reverse().join('');
+    
+    //check removed and revers equal
+    if (removedString === reverseString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
 }
 
