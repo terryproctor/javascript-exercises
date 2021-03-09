12b1c845e803baeed9aa056c23b614282d8d7958
terryproctor <terryproctor2015@outlook.com>
2021-03-09 22:39:21 +0000

    simple caesar , need to have wrap facility see ascii table also (z,1) won't work

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,16 @@
-const caesar = function() {
+caesar = (string, shift) => {
+    let lettersArray = [];
+    let newString = "";
+    for (i=0; i<string.length; i++){
+        lettersArray.push((string.charCodeAt(i)+shift));
+    };
+    for (i=0; i<string.length; i++){
+        newString += String.fromCharCode(lettersArray[i]);
+    };
+    return newString;
+};
 
-}
+console.log(caesar('a', 1));
+
 
 module.exports = caesar
