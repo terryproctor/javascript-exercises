376ec7b3c453ebcee9cf3c883834415e67deb840
terryproctor <terryproctor2015@outlook.com>
2021-03-12 21:50:14 +0000

    slight patch for negative values

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,37 +1,43 @@
-function caesar(string, shift) {
+caesar = (string, shift) => {
     // set variables 
-    let startChar = [];
+    let ascii = [];
+    let alpha = [];
     let newString = "";
+    let x; let y;
 
     //set starting character codes to array
     for (i=0; i<string.length; i++){
-        startChar.push(string.charCodeAt(i));
+        ascii.push(string.charCodeAt(i));
     };
 
-    for (i=0; i<startChar.length; i++) {
-        // A to Z
-        if ((startChar[i] >= 65) && (startChar[i] <= 90)) {
-            // wrap for chars
-            if (((startChar[i] + shift) > 90) || 
-            ((startChar[i] + shift) > 122)) {
-                newString += String.fromCharCode((startChar[i]) + shift - 26);
-            } else {
-            // regular caesar for chars that don't need wrapping
-                newString += String.fromCharCode((startChar[i]) + shift);
-            }
+    for (i = 0; i < ascii.length; i++) {
+      // check if A-Z (65-90) or a-z (97-122)
+    if (((ascii[i] >= 65) && (ascii[i] <= 90)) || 
+    ((ascii[i] >= 97) && (ascii[i] <= 122))) {
+        //set x and y variables to covert ascii to alpha and to new string
+        if ((ascii[i] >= 65) && (ascii[i] <= 90)) {
+            x = 10; y = 64;
+        } else if ((ascii[i] >= 97) && (ascii[i] <= 122)) {
+            x =15; y = 96;
         }
-    
+        let value = ((ascii[i] - x) % 27); 
+        value = (value + shift) 
+        // fix for negative value + shift values
+        if (value < 1) {
+          value = 26 + value;
+        }
+        value = (value % 27) + y;
+        
+        alpha.push(value);
 
-       if ((startChar[i] >= 97) && (startChar[i] <= 122)) {
-            // wrap for uppercase chars
-            if ((startChar[i] + shift) > 122) {
-                newString += String.fromCharCode((startChar[i]) + shift - 26);
-            } else {
-            // regular caesar for chars that don't need wrapping
-                newString += String.fromCharCode((startChar[i]) + shift);
-            }
-        } 
 
+    } else {
+        alpha.push(ascii[i]);
+    };
     }
+    alpha.forEach(element => { 
+        newString += String.fromCharCode(element);     
+    });
+ 
 return newString;
 }
