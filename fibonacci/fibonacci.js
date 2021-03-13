5171661eb6b125bf9539553dc32c9e9d4f6e90d9
terryproctor <terryproctor2015@outlook.com>
2021-03-13 16:22:39 +0000

    final

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = (sequence) => {
+    if (sequence < 0) {
+        return "OOPS";
+    }
+    sequence = Number(sequence);
+    let sequenceArray = [0, ];
 
+    for (let i = 1; i <= sequence; i++) {
+        if (i == 1) {
+          sequenceArray.push(i)
+        } else {
+        sequenceArray.push(sequenceArray[i - 2] + sequenceArray[i - 1])
+        }
+    }
+
+    //console.log(sequenceArray);
+    return sequenceArray.pop();
 }
 
 module.exports = fibonacci
