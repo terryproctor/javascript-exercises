061aee933b8c858e06a2a66555aff2d3b31e06d5
terryproctor <terryproctor2015@outlook.com>
2021-03-14 19:12:44 +0000

    done

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
-
-}
+const findTheOldest = function(array) {
+    return array.reduce((a, b) => {
+      const aAge = getAge(a.yearOfBirth, a.yearOfDeath);
+      const bAge = getAge(b.yearOfBirth, b.yearOfDeath);
+      return aAge > bAge ? a : b;
+    })
+  }
+  
+  const getAge = function(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  }
 
 module.exports = findTheOldest
