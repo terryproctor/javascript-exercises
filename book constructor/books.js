86483deeb467f7dc247e26d94706a26c0a60a3cc
terryproctor <terryproctor2015@outlook.com>
2021-04-10 21:45:31 +0100

    books constructor

--- book constructor/books.js
+++ book constructor/books.js
@@ -1,1 +1,12 @@
-function Book
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = (title, author, pages, read) => {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read': 'not read yet'}`;
+    }
+}
+
+let Charlie = new Book('Charlie and the Chocolate Factory', 'Roal Dahl', 452, true);
+console.log(Charlie.info());
